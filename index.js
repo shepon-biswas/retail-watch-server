6043d2ba64108e6e8ba02e0edf9efdc5ab9e7c07
Shepon Biswas <sheponbiswas01@gmail.com>
2023-07-09 18:53:29 +0600

    Created an API to see details of a single product

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rf3zcjg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,16 +27,25 @@ async function run() {
 
     const productCollection = client.db("retailWatchDB").collection("products");
 
-// Get all products
-    app.get("/products", async(req, res)=>{
-        const result = await productCollection.find().toArray();
-        res.send(result);
-    })
-
+    // Get all products
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+    
+    // Get a single product details
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -45,19 +53,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
 // test api
-app.get("/", (req, res)=>{
-    res.send("server is running...")
-})
+app.get("/", (req, res) => {
+  res.send("server is running...");
+});
 
 // app listen
-app.listen(port, ()=>{
-    console.log("Server is running...")
-})
-
-
-
-
+app.listen(port, () => {
+  console.log("Server is running...");
+});
